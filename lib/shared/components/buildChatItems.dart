f5fdba07ed0132e20872cb011db74d0ab93e89d6
OsamaElnagar <osamaelngar98@gmail.com>
2022-08-18 18:46:32 +0200

    Some improvements on messenger

--- lib/shared/components/buildChatItems.dart
+++ lib/shared/components/buildChatItems.dart
@@ -5,6 +5,206 @@ import 'package:social_app/models/loginModel.dart';
 import 'package:social_app/shared/bloc/AppCubit/cubit.dart';
 import '../styles/iconBroken.dart';
 
+Widget buildSentMessageItem({required ChatsModel chatsModel}) {
+  var date = DateTime.now();
+  String messageDateTime = chatsModel.messageDateTime;
+  messageDateTime = DateFormat.E().add_jm().format(date);
+  // ==> 'Thu, 5/23/2013 10:21:47 AM'
+  return Align(
+    alignment: AlignmentDirectional.topEnd,
+    child: Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+
+        children: [
+          if (chatsModel.textMessage != '' && chatsModel.imageMessage == '')
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.deepPurple[400],
+                  borderRadius: const BorderRadiusDirectional.only(
+                    bottomEnd: Radius.circular(40),
+                    topEnd: Radius.circular(0),
+                    topStart: Radius.circular(10),
+                    bottomStart: Radius.circular(10),
+                  )),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 8.0, right: 20.0, top: 8.0, bottom: 8.0),
+                    child: Text(
+                      chatsModel.textMessage,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 2.0, right: 20.0, top: 8.0, bottom: 8.0),
+                    child: Text(
+                      messageDateTime.substring(3),
+                      style: const TextStyle(
+                          color: Colors.grey, fontSize: 10, height: .5),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          if (chatsModel.textMessage == '' && chatsModel.imageMessage != '')
+            Container(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              decoration: BoxDecoration(
+                  color: Colors.deepPurple[400],
+                  borderRadius: const BorderRadiusDirectional.only(
+                    bottomEnd: Radius.circular(5),
+                    topEnd: Radius.circular(5),
+                    topStart: Radius.circular(5),
+                    bottomStart: Radius.circular(5),
+                  )),
+              child: Column(
+
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    width: 240,
+                    height: 240,
+                    decoration: BoxDecoration(
+                        color: Colors.deepPurple[400],
+                        borderRadius: const BorderRadiusDirectional.only(
+                          bottomEnd: Radius.circular(5),
+                          topEnd: Radius.circular(5),
+                          topStart: Radius.circular(5),
+                          bottomStart: Radius.circular(5),
+                        )),
+                    child: Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Image(
+                        image: NetworkImage('https://th.bing.com/th/id/R.e14667cd86b151776959a6abd2117739?rik=0QwiE8KqcxxOVQ&pid=ImgRaw&r=0'),
+                   fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 2.0, right: 20.0, top: 8.0, bottom: 8.0),
+                    child: Text(
+                      messageDateTime.substring(3),
+                      style: const TextStyle(
+                          color: Colors.grey, fontSize: 10, height: .5),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          if (chatsModel.textMessage != '' && chatsModel.imageMessage != '')
+            Container(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              decoration: BoxDecoration(
+                  color: Colors.deepPurple[400],
+                  borderRadius: const BorderRadiusDirectional.only(
+                    bottomEnd: Radius.circular(5),
+                    topEnd: Radius.circular(5),
+                    topStart: Radius.circular(5),
+                    bottomStart: Radius.circular(5),
+                  )),
+              child: Column(
+
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    width: 240,
+                    height: 240,
+                    decoration: BoxDecoration(
+                        color: Colors.deepPurple[400],
+                        borderRadius: const BorderRadiusDirectional.only(
+                          bottomEnd: Radius.circular(5),
+                          topEnd: Radius.circular(5),
+                          topStart: Radius.circular(5),
+                          bottomStart: Radius.circular(5),
+                        )),
+                    child: Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Image(
+                        image: NetworkImage(chatsModel.imageMessage),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 4.0, right: 8.0, top: 1.0, bottom: 2.0),
+                    child: Text(
+                      chatsModel.textMessage,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 2.0, right: 20.0, top: 8.0, bottom: 8.0),
+                    child: Text(
+                      messageDateTime.substring(3),
+                      style: const TextStyle(
+                          color: Colors.grey, fontSize: 10, height: .5),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget buildReceivedMessageItem({required ChatsModel chatsModel}) {
+  var date = DateTime.now();
+  String messageDateTime = chatsModel.messageDateTime;
+  messageDateTime = DateFormat.E().add_jm().format(date);
+  // ==> 'Thu, 5/23/2013 10:21:47 AM'
+  return Align(
+    alignment: AlignmentDirectional.topStart,
+    child: Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.green[300],
+            borderRadius: const BorderRadiusDirectional.only(
+              bottomEnd: Radius.circular(10),
+              topEnd: Radius.circular(10),
+              topStart: Radius.circular(0),
+              bottomStart: Radius.circular(40),
+            )),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                chatsModel.textMessage,
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+
+            // i will send the time without formating and
+            // when i get it i will formate it before displaying it.
+            //////////////////////////////////////////////////////////////////////////////////////////
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 20.0, right: 2.0, top: 8.0, bottom: 8.0),
+              child: Text(
+                messageDateTime.substring(3),
+                style: const TextStyle(
+                    color: Colors.grey, fontSize: 10, height: .5),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
 Widget buildMessengerFieldItem({
   required TextEditingController messageController,
   required BuildContext context,
@@ -103,7 +303,7 @@ Widget buildMessengerFieldItem({
                   cubit.senMessage(
                     receiverId: loginModel.uId,
                     textMessage: messageText,
-                    messageDateTime:DateTime.now().toLocal().toString(),
+                    messageDateTime: DateTime.now().toLocal().toString(),
                   );
                 }
               }
@@ -115,97 +315,5 @@ Widget buildMessengerFieldItem({
         ),
       ),
     ],
-  );
-}
-
-Widget buildSentMessageItem({required ChatsModel chatsModel}) {
-  var date = DateTime.now();
-  String messageDateTime = chatsModel.messageDateTime;
-  messageDateTime = DateFormat.E().add_jm().format(date);
-  // ==> 'Thu, 5/23/2013 10:21:47 AM'
-  return Align(
-    alignment: AlignmentDirectional.topEnd,
-    child: Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.deepPurple[400],
-            borderRadius: const BorderRadiusDirectional.only(
-              bottomEnd: Radius.circular(40),
-              topEnd: Radius.circular(0),
-              topStart: Radius.circular(10),
-              bottomStart: Radius.circular(10),
-            )),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 8.0, right: 20.0, top: 8.0, bottom: 8.0),
-              child: Text(
-                chatsModel.textMessage,
-                style: const TextStyle(color: Colors.white),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 2.0, right: 20.0, top: 8.0, bottom: 8.0),
-              child: Text(
-                messageDateTime.substring(3),
-                style: const TextStyle(color: Colors.grey,fontSize: 10,height: .5),
-              ),
-            ),
-          ],
-        ),
-      ),
-    ),
-  );
-}
-
-Widget buildReceivedMessageItem({required ChatsModel chatsModel}) {
-  var date = DateTime.now();
-  String messageDateTime = chatsModel.messageDateTime;
-  messageDateTime = DateFormat.E().add_jm().format(date);
-  // ==> 'Thu, 5/23/2013 10:21:47 AM'
-  return Align(
-    alignment: AlignmentDirectional.topStart,
-    child: Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: Container(
-        decoration: BoxDecoration(
-            color: Colors.green[300],
-            borderRadius: const BorderRadiusDirectional.only(
-              bottomEnd: Radius.circular(10),
-              topEnd: Radius.circular(10),
-              topStart: Radius.circular(0),
-              bottomStart: Radius.circular(40),
-            )),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                chatsModel.textMessage,
-                style: const TextStyle(color: Colors.white),
-              ),
-            ),
-
-            // i will send the time without formating and
-            // when i get it i will formate it before displaying it.
-            //////////////////////////////////////////////////////////////////////////////////////////
-            Padding(
-              padding: const EdgeInsets.only(
-                  left:20.0 , right: 2.0, top: 8.0, bottom: 8.0),
-              child: Text(
-                messageDateTime.substring(3),
-                style: const TextStyle(color: Colors.grey,fontSize:10,height: .5 ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    ),
   );
 }
